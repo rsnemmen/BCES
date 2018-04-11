42816efd5a7b3991faa776dc9d597e1de68a3f64
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-04-11 17:19:31 -0300

    improving packaging

---
+++ bces/__init__.py
@@ -0,0 +1,11 @@
+"""
+
+Python module for performing robust linear regression on (X,Y) data points where 
+both X and Y have measurement errors.
+
+The fitting method is the bivariate correlated errors and intrinsic scatter 
+(BCES) and follows the description given in Akritas, M. G., & Bershady, M. A. 
+Astrophysical Journal, 1996, 470, 706. 
+
+.. moduleauthor:: Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
+"""
