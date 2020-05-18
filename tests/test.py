d836dcdb1897afe4bfcdf8111ca492580913d401
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2020-05-18 16:50:48 -0300

    draft of a test script

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,31 @@
 import unittest
-import bces.bces
+import bces.bces as bces
+import numpy as np
 
 class TestBCES(unittest.TestCase):
-    def test_fit_bs(self):
-        """
-        Test BCES fitting with bootstrapping
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
 
+	# reads test dataset
+	data=np.load('../data.npz')
+	xdata=data['x']
+	ydata=data['y']
+	errx=data['errx']
+	erry=data['erry']
+	covdata=data['cov']
+
+	# correct fit parameters expected for dataset
+	ans_pars=np.array([ 0.57955173, 17.88855826])
+	ans_cov=np.array([[ 5.85029731e-04, -2.72808055e-02],
+	       [-2.72808055e-02,  1.27299029e+00]])
 
     def test_fit(self):
-    	"""
-    	Test BCES fitting without bootstrapping
-    	"""
+		"""
+        Test BCES Y|X fit without bootstrapping.
+        """
+        # fit
+        a,b,erra,errb,covab=bces(xdata,errx,ydata,erry,covdata)
 
+        np.testing.assert_array_almost_equal(ans_pars,np.array([a[0],b[0]]))
+        #self.assertEqual(result, 6)
 
 
 
