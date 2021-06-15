eb96021fd83b18b8076f403b4f7e4118e8895b4c
Rodrigo Nemmen da Silva <rsnemmen@gmail.com>
2021-06-15 15:03:24 -0300

    unit tests for further regression methods

--- tests/test_bces.py
+++ tests/test_bces.py
@@ -12,20 +12,49 @@ errx=data['errx']
 erry=data['erry']
 covdata=data['cov']
 
-# correct fit parameters expected for dataset Y|X
-ans_pars=np.array([ 0.57955173, 17.88855826])
-ans_cov=np.array([[ 5.85029731e-04, -2.72808055e-02],
-	[-2.72808055e-02,  1.27299029e+00]])
+# Correct fit parameters expected for dataset. The parameters in 
+# ans_pars are such that y=Ax+B:
+#
+# ans_pars = [ A(y|x), B(y|x), 
+#              A(x|y), B(x|y), 
+#              A(ort), B(ort) ]
+#
+# i.e., each pair contains the expected result for one of the BCES
+# regression methods.
+#
+ans_pars=np.array([ [0.57955173, 17.88855826], 
+	                [0.26053751, 32.70952271], 
+	                [0.50709256, 21.25491222] ])
+# covariance matrix
+ans_cov=np.array([[ 5.85029731e-04, -2.72808055e-02], 
+				  [-2.72808055e-02,  1.27299029e+00]])
 
 def test_yx():
 	"""
 	Test BCES Y|X fit without bootstrapping.
 	"""
 	# fit
-	#a,b,erra,errb,covab=B.bcesp(xdata,errx,ydata,erry,covdata)
 	a,b,erra,errb,covab=BCES.bces(xdata,errx,ydata,erry,covdata)
 
-	np.testing.assert_array_almost_equal(ans_pars,np.array([a[0],b[0]]))
+	np.testing.assert_array_almost_equal([ans_pars[0,0],ans_pars[0,1]], np.array([a[0],b[0]]))
+
+def test_xy():
+	"""
+	Test BCES X|Y fit without bootstrapping.
+	"""
+	# fit
+	a,b,erra,errb,covab=BCES.bces(xdata,errx,ydata,erry,covdata)
+
+	np.testing.assert_array_almost_equal([ans_pars[1,0],ans_pars[1,1]], np.array([a[1],b[1]]))
+
+def test_ort():
+	"""
+	Test BCES orthogonal fit without bootstrapping.
+	"""
+	# fit
+	a,b,erra,errb,covab=BCES.bces(xdata,errx,ydata,erry,covdata)
+
+	np.testing.assert_array_almost_equal([ans_pars[2,0],ans_pars[2,1]], np.array([a[3],b[3]]))
 
 def test_yxboot():
 	"""
@@ -35,7 +64,17 @@ def test_yxboot():
 	a,b,erra,errb,covab=BCES.bcesp(xdata,errx,ydata,erry,covdata)
 
 	# check if the regression parameters match within 1 decimal
-	np.testing.assert_array_almost_equal(ans_pars,np.array([a[0],b[0]]),1)
+	np.testing.assert_array_almost_equal([ans_pars[0,0],ans_pars[0,1]], np.array([a[0],b[0]]),1)
+
+def test_ortboot():
+	"""
+	Test BCES orthogonal fit with bootstrapping.
+	"""
+	# fit
+	a,b,erra,errb,covab=BCES.bcesp(xdata,errx,ydata,erry,covdata)
+
+	# check if the regression parameters match within 1 decimal
+	np.testing.assert_array_almost_equal([ans_pars[2,0],ans_pars[2,1]], np.array([a[3],b[3]]),1)
 
 def test_bootstrap():
 	"""
