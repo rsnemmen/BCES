6d5d159c0169c4757ddf01aebe384f7ff7a1113f
Rodrigo Nemmen da Silva <rsnemmen@gmail.com>
2021-06-22 14:51:31 -0300

    added new requirement

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('LICENSE') as f:
 
 setup(
     name='bces',
-    version='1.3',
+    version='1.3.1',
     description='Python module for performing linear regression for data with measurement errors and intrinsic scatter',
     long_description=readme,
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/BCES',
-    download_url = 'https://github.com/rsnemmen/BCES/archive/1.3.tar.gz',
+    download_url = 'https://github.com/rsnemmen/BCES/archive/1.3.1.tar.gz',
     license=license,
-    keywords = ['statistics', 'fitting', 'linear-regression'],
+    keywords = ['statistics', 'fitting', 'linear-regression','machine-learning'],
     packages=find_packages(exclude=('tests', 'docs'))
 )
