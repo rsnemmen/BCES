3f084a1c0a9a0d7ff904d37a0221340fdf41cb63
Rodrigo Nemmen da Silva <rsnemmen@gmail.com>
2023-03-03 10:36:19 -0800

    fixed PyPI issue

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open('LICENSE') as f:
 
 setup(
     name='bces',
-    version='1.5',
+    version='1.5.1',
     description='Python module for performing linear regression for data with measurement errors and intrinsic scatter',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/BCES',
-    download_url = 'https://github.com/rsnemmen/BCES/archive/1.5.tar.gz',
+    download_url = 'https://github.com/rsnemmen/BCES/archive/1.5.1.tar.gz',
     license=license,
     keywords = ['statistics', 'fitting', 'linear-regression','machine-learning'],
     packages=find_packages(exclude=('tests', 'docs'))
